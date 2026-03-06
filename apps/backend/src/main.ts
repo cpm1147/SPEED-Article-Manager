0ef5217328c5755ffb5c59c2e9102b3506a764a7
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-06 23:08:03 +1300

    update main.ts for render

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,27 +1,12 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
-const server = express();
-
-async function createNestServer(expressInstance = server) {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: true });
-  await app.init();
+  await app.listen(process.env.PORT ?? 3001);
 }
-
-if (process.env.VERCEL) {
-  createNestServer(server);
-} else {
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({ origin: true, credentials: true });
-    await app.listen(process.env.PORT || 3001);
-    console.log('Local backend running');
-  }
-  bootstrap();
-}
-
-export default server;
+bootstrap();
